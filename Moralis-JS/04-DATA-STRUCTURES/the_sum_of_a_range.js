f79c47ae7a7434c992f5991f0202cde9841443ef
Filip Brouwers <filip.brouwers@gmail.com>
2022-01-17 17:53:35 +0100

    change of structure

--- Moralis-JS/04-DATA-STRUCTURES/the_sum_of_a_range.js
+++ Moralis-JS/04-DATA-STRUCTURES/the_sum_of_a_range.js
@@ -3,8 +3,23 @@
 
 // write range function, 2 arguments: start and end, returns array [start ... end]
 
-function range(start_value, end_value)
+function range(start_value, end_value) {
   let array_of_numbers = [];
   for (let number = start_value; number === end_value; number += 1) {
     array_of_numbers.push(number)
   }
+  array_of_numbers;
+}
+
+
+
+// browser try-out
+
+// const range = function(start_value, end_value) {
+//   let array_of_numbers = [];
+//   for (let number = start_value; number < end_value; number = number + 1) {
+//     array_of_numbers.push(number)
+//   }
+//   console.log(array_of_numbers);
+// }
+// range(1, 5);
