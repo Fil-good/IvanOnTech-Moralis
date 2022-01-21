d90cd07269c0a19bbcd7d9b3653adc10a94427d6
Filip Brouwers <filip.brouwers@gmail.com>
2022-01-21 11:14:29 +0100

    running the file in terminal with node

--- Moralis-JS/04-DATA-STRUCTURES/the_sum_of_a_range.js
+++ Moralis-JS/04-DATA-STRUCTURES/the_sum_of_a_range.js
@@ -1,15 +1,20 @@
-// starting from console.log(sum(range(1, 10)));
+// in order to run this file, go to the terminal and type 'node file.js'
 
-
-// write range function, 2 arguments: start and end, returns array [start ... end]
+/* starting from console.log(sum(range(1, 10)));
+write range function, 2 arguments: start and end, returns array [start ... end]
+*/
 
 function range(start_value, end_value) {
   let array_of_numbers = [];
-  for (let number = start_value; number === end_value; number += 1) {
-    array_of_numbers.push(number)
+  for (let number = start_value; number <= end_value; number += 1) {
+    array_of_numbers.push(number);
   }
-  array_of_numbers;
+  console.log(`yo! ${array_of_numbers}`);
 }
+
+console.log("test");
+console.log(range(0,5));
+
 
 
 
