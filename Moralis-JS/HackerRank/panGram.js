1d3fe430abfc794b616aee86fe5b0b1b5ff10438
Filip Brouwers <filip.brouwers@gmail.com>
2022-02-09 12:24:42 +0100

    hackerRank doesnt take this solution, but functional

--- Moralis-JS/HackerRank/panGram.js
+++ Moralis-JS/HackerRank/panGram.js
@@ -29,22 +29,25 @@ function readLine() {
  * The function accepts STRING s as parameter.
  */
 
+// no break; possible in forEach loop!
+
 function pangrams(s) {
   // Write your code here
   const alpha = Array.from(Array(26)).map((e, i) => i + 65);
   const alphabet = alpha.map((x) => String.fromCharCode(x));
+  let S = s.toUpperCase();
+  let res = '';
 
-  const regex = /A-z/g;
-  for (a of regex) {
-    if (s.find(a) == true) {
-      return 'pangram';
+  for (letter of alphabet) {
+    if (S.includes(letter)) {
+      res = 'pangram';
     } else {
-      return 'not pangram';
+      res = 'not pangram';
       break;
     }
   }
 
-
+  console.log(res);
 }
 
 function main() {
