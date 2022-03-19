b8bbd50ee0ffe59511813e372a9594969c77239a
Filip Brouwers <filip.brouwers@gmail.com>
2022-03-19 15:00:28 +0100

    first try sparse arrays

---
+++ Moralis-JS/HackerRank/sparseArrays.js
@@ -0,0 +1,24 @@
+function matchingStrings(strings, queries) {
+  // Write your code here
+  let n = strings.length;
+  let q = queries.length;
+// you would need an array of counters, length = q
+  let counters = new Array(q).fill("");
+  let countersInt = new Int8Array(counters);
+  for(let j=0; j<q; j++) {
+    for(let i=0; i<n; i++) {
+      if(strings[i] === queries[j]) {
+        countersInt[i] += 1;
+      }
+    }
+  }
+  console.log(countersInt);
+
+
+
+}
+
+
+let strs = ["a", "abcd", "a"];
+let qrs = ["a", "abcd", "abcdefgh"];
+matchingStrings(strs,qrs)
