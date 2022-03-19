4516c9c7a6b8819dd6149363504fe6ad683b0f07
Filip Brouwers <filip.brouwers@gmail.com>
2022-03-19 22:44:36 +0100

    breakme for choosing what to break

---
+++ Moralis-JS/HackerRank/lonelyInt.js
@@ -0,0 +1,31 @@
+function lonelyinteger(a) {
+  // Write your code here
+  let sortedA = new Uint8Array(a).sort();
+  // console.log(sortedA);
+  let n = sortedA.length;
+
+  let lonely = 0;
+
+  // with breakme you can choose what to break
+  // still one testcase on hackerrank that doesn't pass (case of n=1 I guess)
+
+  breakme: for (let i = 0; i < (n-2); i+=2) {
+    console.log(`i before if: ${i}`);
+    if (sortedA[i] != sortedA[i+1]) {
+      console.log(`i after if: ${i}`);
+      lonely = (sortedA[i]);
+      // console.log(lonely);
+      break breakme;
+    } else {
+      lonely = sortedA[n-1];
+    }
+  }
+console.log(lonely);
+};
+
+// console.log(lonely);
+// if(lonely==0) {console.log(sortedA[n-1])}
+
+// n=7, should iterate to 5th element, index 4
+let aa = [1];
+lonelyinteger(aa)
